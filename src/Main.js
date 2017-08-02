bf5170129de0651641a23d768adda1a150f673fa
Dong Hu <cdhu@uwaterloo.ca>
2017-08-02 14:38:37 -0400

    Set up development environment for react native ios

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Main extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>Hey Text App</Text>
+      </View>
+    )
+  }
+}
+
+export default Main;
